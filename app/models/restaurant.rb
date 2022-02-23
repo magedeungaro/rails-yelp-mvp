2918e45d28e4ec4e41f317290aafac7d8fc0493b
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-02-23 14:42:11 -0300

    valdations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,17 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :address, presence: true
+  validates :name, presence: true
+  validates :category, inclusion: { in: :categories_list }
+
+  def self.categories_list
+    %w[chinese italian japanese french belgian]
+  end
+
+  private
+
+  def categories_list
+    self.class.categories_list
+  end
 end
