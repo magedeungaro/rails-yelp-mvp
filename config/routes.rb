19689613197fe06ad08ad3c41ecd5d8b6140b7b2
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-02-23 17:08:03 -0300

    refactored

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'restaurants#index'
 
   resources :restaurants, only: %i[index new create show] do
-    resources :reviews, only: %i[new create]
+    resources :reviews, only: %i[create]
   end
 end
