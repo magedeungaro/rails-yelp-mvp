afad4475d83622a5fc37111ecfb9bfe215100877
Maria Eugenia Deungaro Vezza <maria.borgato@alumni.usp.br>
2022-02-23 15:35:34 -0300

    footer and header

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'restaurants#index'
+
   resources :restaurants, only: %i[index new create] do
     resources :reviews, only: %i[new create]
   end
